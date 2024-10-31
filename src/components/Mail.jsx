b61947b2c41fe72ed3b33c800bcef5bcc01a13f5
saksham-x <sakshampokharel2002@gmail.com>
2024-10-31 14:43:49 +0545

    Message box

--- src/components/Mail.jsx
+++ src/components/Mail.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
 import { BsTrash } from 'react-icons/bs';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa6';
+import { GoReply } from 'react-icons/go';
 import { IoMdArrowBack, IoMdMore } from 'react-icons/io'
-import { MdMarkAsUnread, MdOutlineArchive, MdOutlineDriveFileMove } from "react-icons/md";
+import { IoStarOutline } from 'react-icons/io5';
+import { MdArrowDropDown, MdMarkAsUnread, MdMore, MdOutlineArchive, MdOutlineDriveFileMove, MdOutlineReply, MdOutlineStar } from "react-icons/md";
 import { RiSpamLine } from 'react-icons/ri';
+import { TfiNewWindow } from "react-icons/tfi";
+import { useNavigate } from 'react-router-dom';
+
 
 const Mail = () => {
+    const navigate = useNavigate();
     return (
-        <div className='flex-1 bg-white rounded-xl mx-5 shadow-lg'>
+        <div className='flex-1 bg-white rounded-xl mx-6 shadow-lg'>
 
 
             {/* icons on mail top bar */}
             <div className='flex items-center justify-between px-4'>
 
                 <div className='flex items-center gap-3'>
-                    <div className='hover:bg-gray-200 hover:rounded-md p-3'>
+                    <div onClick={() => navigate("/")} className='hover:bg-gray-200 hover:rounded-md p-3'>
                         <IoMdArrowBack size={"20px"} />
                     </div>
                     <div className='hover:bg-gray-200 hover:rounded-md p-3'>
@@ -42,12 +48,52 @@ const Mail = () => {
                     <div className='p-2 hover:bg-gray-200 hover:rounded-md cursor-pointer'> <FaChevronRight size={"20px"} /></div>
                 </div>
             </div>
-            <div>
+            {/* 2nd navbar  */}
+            <div className='flex items-center justify-between'>
                 <div className='flex items-center gap-3 px-4 ml-8 my-3'>
-
                     <h1 className='text-xl'>Subject</h1> <p className='text-gray-500 bg-gray-300 px-2 rounded font-extralight text-xs'> Inbox </p>
-
                 </div>
+                <div className='mr-5'>
+                    <div className='cursor-pointer p-2 hover:bg-gray-200 hover:rounded-md '>
+                        <TfiNewWindow size={"20px"} />
+                    </div>
+                </div>
+            </div>
+            {/* third navbar  */}
+            <div className='flex px-4 justify-between'>
+                <div className='  p-2 gap-3 text-gray-600'>
+
+                    <div className='flex items-center'>
+                        <h1 className='cursor-pointer'>Name</h1> &nbsp;
+                        <p className='text-xs'> {"<"}jobs-noreply@linkedin.com{">"} </p>
+
+                        <button className='text-xs text-blue-400 hover:bg-gray-300 hover:rounded-md p-1 hover:text-blue-700'>unsubscribe</button>
+                    </div>
+                    <div className='flex items-center text-xs'>
+                        to me  <MdArrowDropDown size={"20px"} />
+                    </div>
+                </div>
+
+                {/* right side icons  */}
+                <div className='flex items-center gap-2 text-gray-600 text-xs'>
+                    <div className='p-1 cursor-pointer rounded-md hover:bg-gray-200'>
+                        Mon, Oct 21, 2:33 PM (10 days ago)
+                    </div>
+                    <div className='p-1  cursor-pointerrounded-md hover:bg-gray-200'>
+                        <IoStarOutline />
+                    </div>
+                    <div className='p-1 cursor-pointer rounded-md hover:bg-gray-200'>
+                        <GoReply />
+                    </div>
+                    <div className='p-1 cursor-pointer rounded-md hover:bg-gray-200'>
+                        <IoMdMore />
+                    </div>
+                </div>
+            </div>
+            {/* Message  */}
+
+            <div className='py-10 mx-6'>
+                MEssage
             </div>
         </div>
     )
---
+++ src/components/SendMail.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SendMail = () => {
+    return (
+        <div>
+            Send Mailgit
+        </div>
+    )
+}
+
+export default SendMail
